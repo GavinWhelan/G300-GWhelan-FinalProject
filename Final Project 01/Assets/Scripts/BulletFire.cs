7650c444ef58d5035cb7e0e1c57de57d97a4ea05
Gavin Whelan <gavinpwhelan@gmail.com>
2019-04-02 03:59:22 -0400

    More work on gravity well mechanics

--- Final Project 01/Assets/Scripts/BulletFire.cs
+++ Final Project 01/Assets/Scripts/BulletFire.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class BulletFire : MonoBehaviour
 {
     public float speed;
+    private Vector3 pullDirection;
+
+    private Vector3 bulletVelocity;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward;
+        bulletVelocity = GetComponent<Rigidbody>().velocity;
+        pullDirection = new Vector3(0.0f, 0.0f, 0.0f);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Rigidbody>().velocity = transform.forward * speed;
-        GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Impulse);
+        bulletVelocity += pullDirection;
+        GetComponent<Rigidbody>().velocity = (bulletVelocity) * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Gravity Well")
+        {
+            pullDirection = other.transform.position - transform.position;
+            pullDirection *= 0.1f;
+        }
     }
 }
