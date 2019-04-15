9bacf02de3f53d239d21b82a05b7352a37f76af7
ADS\gpwhelan <gavinpwhelan@gmail.com>
2019-04-15 12:28:29 -0400

    More work on new gun, incl. addition of lineDrop objects dropped along each of the aimPoints

--- Final Project 01/Assets/Scripts/New Gun/NewGunPlaCon.cs
+++ Final Project 01/Assets/Scripts/New Gun/NewGunPlaCon.cs
@@ -15,7 +15,9 @@ public class NewGunPlaCon : MonoBehaviour
     public GameObject aimLine;
     public Transform shotSpawn;
     public GameObject gun;
-    public GameObject aimPointObject;
+    public GameObject lineDrop;
+
+    LineRenderer line;
 
     public Transform aimSight;
 
@@ -27,7 +29,6 @@ public class NewGunPlaCon : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        LineRenderer line = shotSpawn.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -58,13 +59,17 @@ public class NewGunPlaCon : MonoBehaviour
 
         if (isFiring && AimPoints.Count <= maxCount)
         {
+            Debug.Log("Here");
             AimPoints.Add(AimPoints.Count + 1, aimLine.GetComponent<Transform>().position);
-            
+            Instantiate(lineDrop, AimPoints[AimPoints.Count], aimLine.GetComponent<Transform>().rotation);
+            LineRenderer line = lineDrop.GetComponent<LineRenderer>();
+            line.SetPosition(0, AimPoints[(AimPoints.Count) - 1]);
+            line.SetPosition(1, AimPoints[AimPoints.Count]);
         }
 
         if (AimPoints.Count > -1)
         {
-            Debug.Log(AimPoints);
+            //Debug.Log(AimPoints);
         }
 
         if (Input.GetButton("Fire1") && AimPoints.Count == 0)
