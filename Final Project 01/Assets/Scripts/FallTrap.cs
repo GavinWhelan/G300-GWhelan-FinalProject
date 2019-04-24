1cf98f58c576271ecdb72a1a253216650fbc50d1
Gavin Whelan <gavinpwhelan@gmail.com>
2019-04-24 03:42:24 -0400

    Created pillar, added assets, modified FallTrap script

---
+++ Final Project 01/Assets/Scripts/FallTrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTrap : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            other.transform.rotation = Quaternion.identity;
+        }
+
+        if(other.tag == "Draggable")
+        {
+            other.transform.position = other.GetComponent<SpawnPosition>().spawnPosition.transform.position;
+            other.transform.rotation = Quaternion.identity;
+            other.GetComponent<Rigidbody>().velocity = new Vector3 (0.0f, 0.0f, 0.0f);
+        }
+    }
+}
